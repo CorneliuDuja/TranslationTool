46018fac624142008aaccf2dccc73a804a2dc38f
Corneliu Duja <corneliu.duja@codefactorygroup.com>
2016-01-25 15:37:45 +0200

    Version N2

--- TranslationTool/TranslationTool.Interface/TranslationTool.Interface.Web/controllers/searchController.js
+++ TranslationTool/TranslationTool.Interface/TranslationTool.Interface.Web/controllers/searchController.js
@@ -1,21 +1,22 @@
 /**
  * Created by user on 1/20/16.
  */
-(function(){
-    var searchController = funtion($scope){
+angular.module('sortApp', [])
 
-    }
-    function getColumnHeaders(){
-        $scope.columnHeaders = [];
-        $scope.columnHeaders.push('Key');
-        $scope.columnHeaders.push('Application');
-        $scope.columnHeaders.push('Branches');
-        $scope.columnHeaders.push('Value');
+    .controller('searchController', function($scope) {
+        $scope.sortType     = 'key'; // set the default sort type
+        $scope.sortReverse  = false;  // set the default sort order
+        $scope.searchKey   = '';     // set the default search/filter term
 
-        }
+        // create the list of sushi rolls
+        $scope.find = [
+            { key: 'action.refund.item', application: 'POS', branch: '14.1-MAINT', value: 'Refundovat polozku', check: 'check' },
+            { key: 'action.refund.item', application: 'CO', branch: '14.1-MAINT', value: 'Refundovat polozku', check: 'check'  },
+            { key: 'column.item.id', application: 'CO', branch: '15.1-MAINT', value: 'Refundovat polozku' , check: 'check' },
+            { key: 'palette.suspend.order', application: 'CO', branch: '14.1-MAINT', value: 'Refundovat polozku', check: 'check'  }
+        ];
 
-    searchController.$inject = ['$scope'];
-}());
+    });
 
 
 
--- TranslationTool/TranslationTool.Interface/TranslationTool.Interface.Web/controllers/uploadController.js
+++ TranslationTool/TranslationTool.Interface/TranslationTool.Interface.Web/controllers/uploadController.js
@@ -1,12 +1,44 @@
 /**
  * Created by user on 1/20/16.
  */
-$scope.add = function(){
-    var f = document.getElementById('file').files[0],
-        r = new FileReader();
-    r.onloadend = function(e){
-        var data = e.target.result;
-        //send you binary data via $http or $resource or do anything else with it
+var app = angular.module('app', [], function() {})
+app.controller('UploadController', function($scope)
+{
+    $scope.setFiles = function(element) {
+        $scope.$apply(function($scope) {
+            console.log('files:', element.files);
+            // Turn the FileList object into an Array
+            $scope.files = []
+            for (var i = 0; i < element.files.length; i++) {
+                $scope.files.push(element.files[i])
+            }
+            $scope.progressVisible = false
+        });
+    };
+
+    function uploadProgress(evt) {
+        $scope.$apply(function(){
+            if (evt.lengthComputable) {
+                $scope.progress = Math.round(evt.loaded * 100 / evt.total)
+            } else {
+                $scope.progress = 'unable to compute'
+            }
+        })
     }
-    r.readAsBinaryString(f);
-}
+
+    function uploadComplete(evt) {
+        /* This event is raised when the server send back a response */
+        alert(evt.target.responseText)
+    }
+
+    function uploadFailed(evt) {
+        alert("There was an error attempting to upload the file.")
+    }
+
+    function uploadCanceled(evt) {
+        $scope.$apply(function(){
+            $scope.progressVisible = false
+        })
+        alert("The upload has been canceled by the user or the browser dropped the connection.")
+    }
+});
